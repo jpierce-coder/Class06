d1ab1b80c14a6b4904cb448e28f303f439a642ad
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-09-02 00:11:13 -0500

    Update scriptDemo.js

--- Demo/scriptDemo.js
+++ Demo/scriptDemo.js
@@ -1,16 +1,18 @@
-const cityInputElem = document.querySelector('input');
+const cityInputElem = document.querySelector("input");
 const btnSearch = document.getElementById('searchBtn');
 
-btnSearch.addEventListener('click', fetchWeather);
+btnSearch.addEventListener('click', fetchWeather)
+
 
 
 function fetchWeather(event) {
-    event.preventDefault();
-    let cityName = cityInputElem.value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`
-    ).then((resp) => {
-        return resp.json();
-    }).then((weather) => {
-        console.log(weather)
-    })
+  event.preventDefault();
+  let cityName = cityInputElem.value;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`
+  ).then((resp)=>{
+    return resp.json();
+  }).then((weather)=>{
+    console.log(weather)
+  })
 }
