fb4325d67d689e47b4c7c94e6eff731c74c63932
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-09-06 18:24:58 -0500

    Run Another Test Product Function
    Ran another test run of function Product

--- exercise-6/exercise6.js
+++ exercise-6/exercise6.js
@@ -7,4 +7,6 @@ function product([[first, second], [third, fourth], [fifth, sixth]]) {
 
 // product([[1, 2], [3, 4], [5, 6]]);
 
-product([[5, 4], [7, 6], [9, 8]]);
+// product([[5, 4], [7, 6], [9, 8]]);
+
+product([[7, 4], [2, 3], [6, 5]]);
